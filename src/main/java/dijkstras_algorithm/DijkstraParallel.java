410c9764581b26e578fd3063e453a9e4cee9c745
dowdyma1 <dowdyma1@gmail.com>
2022-03-21 10:09:59 -0400

    update dijkstra parallel for new algorithm

--- src/main/java/dijkstras_algorithm/DijkstraParallel.java
+++ src/main/java/dijkstras_algorithm/DijkstraParallel.java
@@ -1,48 +1,72 @@
 package dijkstras_algorithm;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
-public class DijkstraParallel extends DijkstraCore {
+public class DijkstraParallel {
     private final Set<Thread> threads;
-    private final AtomicInteger numActiveThreads;
+    protected final ThreadLocal<Map<Node, Integer>> dist;
+    protected final ThreadLocal<Map<Node, Node>> prev;
+    protected final ThreadLocal<Queue<Node>> queue;
+    protected final ThreadLocal<Set<Node>> settledNodes;
+    protected final ConcurrentMap<Node, Integer> decideGlobalMin;
+    protected final AtomicMarkableReference<Node> globalMinimumNode;
+    protected final AtomicBoolean previousAuthorityToggle;
+    protected final AtomicInteger numActiveThreads;
+    protected final Set<Node> nodes;
+    protected final Node source;
 
     public DijkstraParallel(Set<Node> nodes, Node source) {
-        super(nodes, source);
-        dist = new HashMap<>();
-        prev = new HashMap<>();
-        queue = new LinkedList<>();
-        threads = new HashSet<>();
+        dist = ThreadLocal.withInitial(HashMap::new);
+        prev = ThreadLocal.withInitial(HashMap::new);
+        queue = ThreadLocal.withInitial(LinkedList::new);
+        settledNodes = ThreadLocal.withInitial(HashSet::new);
 
-        numActiveThreads = new AtomicInteger(nodes.size());
-        for(Node node: nodes){
-            DijkstraSSSP sssp = new DijkstraSSSP(node, numActiveThreads);
-            Thread thread = new Thread(sssp);
-            threads.add(thread);
+        this.nodes = nodes;
+        this.source = source;
+        threads = new HashSet<>();
+        decideGlobalMin = new ConcurrentHashMap<>();
+        globalMinimumNode = new AtomicMarkableReference<>(source, true);
+        previousAuthorityToggle = new AtomicBoolean(true);
+        numActiveThreads = new AtomicInteger(Runtime.getRuntime().availableProcessors());
+
+        int numNodesPerPartition = (int) Math.floor(nodes.size()/numActiveThreads.get());
+        LinkedList<Node> tempNodes = new LinkedList<>(nodes);
+
+        for(int i = 0; i < numActiveThreads.get(); i++){
+            // evenly distribute nodes across all partitions
+            Set<Node> partitionNodes = new HashSet<>();
+            for(int j = 0; j < numNodesPerPartition; j++){
+                if(!tempNodes.isEmpty()){
+                    partitionNodes.add(tempNodes.remove());
+                }
+            }
+            threads.add(new Thread(new Partition(i == 0 ? true : false, partitionNodes, numActiveThreads)));
         }
     }
 
-    private class DijkstraSSSP implements Runnable{
-        private final Node target;
-        private final Map<Node, Integer> localDist; // distance from source to key
-        private final Map<Node, Node> localPrev; // value is next node on the path from the key Node to the source
-        private final Queue<Node> localQueue;
+    private class Partition implements Runnable{
+        private final boolean isGlobalAuthority;
+        private final Set<Node> localNodes;
+        private final int numThreads;
         private final AtomicInteger numActiveThreads;
 
-        public DijkstraSSSP(Node target, AtomicInteger numActiveThreads){
-            this.target = target;
+        public Partition(boolean isGlobalAuthority, Set<Node> localNodes, AtomicInteger numActiveThreads){
+            this.isGlobalAuthority = isGlobalAuthority;
+            this.localNodes = localNodes;
+            this.numThreads = numActiveThreads.get();
             this.numActiveThreads = numActiveThreads;
-            localDist = new HashMap<>();
-            localPrev = new HashMap<>();
-            localQueue = new LinkedList<>();
-
         }
 
-        private Node getShortestDistance(Queue<Node> queue){
+        private Node extractLocalMin(){
             Node curNode = null;
             int smallestDist = Integer.MAX_VALUE;
-            for(Node node: queue){
-                int curDist = localDist.get(node);
+            for(Node node: queue.get()){
+                int curDist = dist.get().get(node);
                 if(curDist < smallestDist){
                     smallestDist = curDist;
                     curNode = node;
@@ -51,42 +75,76 @@ public class DijkstraParallel extends DijkstraCore {
             return curNode;
         }
 
+        private Node extractGlobalMin(Node localMin){
+            if(isGlobalAuthority){
+                // busy wait until the only active thread is the Authority
+                while(numActiveThreads.get() > 1){}
+                Integer globalMin = dist.get().get(localMin);
+                Node minNode = localMin;
+
+                // iterate over each thread's local minimum node to find global minimum
+                for(Map.Entry<Node, Integer> entry : decideGlobalMin.entrySet()){
+                    if(entry.getValue() < globalMin){
+                        globalMin = entry.getValue();
+                        minNode = entry.getKey();
+                    }
+                }
+                // set the global minimum node and set the mark to be the opposite of the previousAuthorityToggle
+                globalMinimumNode.set(minNode, !previousAuthorityToggle.get());
+                return minNode;
+            }
+            else{
+                // previous status of Authority
+                boolean prevAuth = previousAuthorityToggle.get();
+
+                // Give Authority thread's local minimum node
+                decideGlobalMin.put(localMin, dist.get().get(localMin));
+                // set thread to inactive state
+                numActiveThreads.decrementAndGet();
+
+                // busy wait until Authority has finished finding global minimum node
+                while(globalMinimumNode.isMarked() == prevAuth){}
+                return globalMinimumNode.getReference();
+            }
+        }
+
         @Override
         public void run() {
-            for(Node node: nodes){
-                localDist.put(node, Integer.MAX_VALUE);
-                localPrev.put(node, null);
-                localQueue.add(node);
+            for(Node node: localNodes){
+                dist.get().put(node, Integer.MAX_VALUE);
+//                prev.get().put(node, null);
+//                queue.get().add(node);
             }
-            localDist.put(source, 0);
+            if(localNodes.contains(source)){
+                settledNodes.get().add(source);
+                dist.get().put(source, 0);
+                queue.get().add(source);
+            }
+            boolean quit = false;
 
-            while(!localQueue.isEmpty()){
-                Node curNode = getShortestDistance(localQueue);
-                if(curNode.equals(target)){
-                    dist.put(target, localDist.get(curNode));
-                    prev.put(target, localPrev.get(curNode));
-                    numActiveThreads.decrementAndGet();
-                    return;
-                }
+//            while(!quit){
+//
+//            }
+            while(!queue.get().isEmpty()){
+                Node curNode = extractGlobalMin(extractLocalMin());
 
-                localQueue.remove(curNode);
+                if(localNodes.contains(curNode)){
+                    settledNodes.get().add(curNode);
 
-                for(Node neighbor: curNode.getNeighbors()){
-                    int distFromRoot = localDist.get(curNode) + curNode.getDistanceToNeighbor(neighbor);
+                    for(Node neighbor: curNode.getNeighbors()){
+                        int distFromRoot = dist.get().get(curNode) + curNode.getDistanceToNeighbor(neighbor);
 
-                    if(distFromRoot < localDist.get(neighbor)){
-                        localDist.put(neighbor, distFromRoot);
-                        localPrev.put(neighbor, curNode);
+                        if(distFromRoot < dist.get().get(neighbor)){
+                            dist.get().put(neighbor, distFromRoot);
+                            prev.get().put(neighbor, curNode);
+                        }
                     }
                 }
             }
-
-            dist.put(target, Integer.MAX_VALUE);
             numActiveThreads.decrementAndGet();
         }
     }
 
-    @Override
     public void run() {
         for(Thread thread: threads){
             thread.start();
